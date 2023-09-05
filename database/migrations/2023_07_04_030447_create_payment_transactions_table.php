e7e3f3f9eb18039683016edbed6d61117fbe4250
Niwantha <niwantha@efuturesworld.com>
2023-09-05 09:57:24 +0530

    change reference type migration

--- database/migrations/2023_07_04_030447_create_payment_transactions_table.php
+++ database/migrations/2023_07_04_030447_create_payment_transactions_table.php
@@ -16,6 +16,7 @@ class CreatePaymentTransactionsTable extends Migration
         Schema::create('payment_transactions', function (Blueprint $table) {
             $table->bigIncrements('transaction_id');
             $table->integer('reference_id');
+            $table->enum('reference_type', ['INTERNAL', 'EXTERNAL'])->default('INTERNAL');
             $table->integer('user_id');
             $table->string('description');
             $table->float('amount');
